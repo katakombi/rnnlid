a1c958351462e9d6678b6ad9aa261dbbe92ffd30
katakombi <katakombi@gmail.com>
2013-07-21 10:40:08 +0200

    train data was too large, reducing it to 1/10th

--- data/split.sh
+++ data/split.sh
@@ -14,5 +14,6 @@ mv xat $DS.test3
 
 mv xae $DS.valid
 
+../genvocab | cat - xam > $DS.train1
 ../genvocab | cat - xab xac xad xaf xag xah xai xak xal xam xan xao xap xaq xar xas > $DS.train
 rm -f xab xac xad xaf xag xah xai xak xal xam xan xao xap xaq xar xas
--- train/train.sh
+++ train/train.sh
@@ -5,7 +5,7 @@ cd $P/rnnlm-0.3e
 make
 cd ..
 
-time rnnlm-0.3e/rnnlm -rnnlm EP-30h-0.03lr.de.rnn -debug 2 -hidden 30 -alpha 0.03 -train ../data/EP.de-en.de.txt.train -valid ../data/EP.de-en.de.txt.valid -bptt 5 -class 16
+time rnnlm-0.3e/rnnlm -rnnlm EP-30h-0.03lr.de.rnn -debug 2 -hidden 30 -alpha 0.03 -train ../data/EP.de-en.de.txt.train1 -valid ../data/EP.de-en.de.txt.valid -bptt 5 -class 16
 
 #
 # required EP-30h-0.03lr.de.rnn.output.txt output
